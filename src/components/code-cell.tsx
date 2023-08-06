9aa61b34e58d4798ccbb0335c679b8b40705a418
ldco2016 <renaissance.scholar2012@gmail.com>
2023-08-06 10:46:22 -0500

    first commit

---
+++ src/components/code-cell.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+import CodeEditor from "./code-editor";
+import Preview from "./preview";
+import bundle from "../bundler";
+import Resizable from "./resizable";
+
+const CodeCell = () => {
+  const [code, setCode] = useState("");
+  const [error, setError] = useState("");
+  const [input, setInput] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output.code);
+      setError(output.error);
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
+
+  return (
+    <Resizable direction='vertical'>
+      <div style={{ height: "100%", display: "flex", flexDirection: "row" }}>
+        <Resizable direction='horizontal'>
+          <CodeEditor
+            initialValue={
+              "import React, { useState } from 'react';\nimport { render, screen } from '@testing-library/react';"
+            }
+            onChange={(value) => setInput(value)}
+          />
+        </Resizable>
+        <Preview code={code} error={error} />;
+      </div>
+    </Resizable>
+  );
+};
+
+export default CodeCell;
